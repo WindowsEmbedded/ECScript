002a56753682ef79b2c7d3a827a1f056772dd00e
WindowsEmbedded <embedded09@outlook.com>
2023-02-12 18:33:29 +0800

    实现print的一半

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,21 +10,42 @@
 
 #include <map> //dict类型
 #include <vector>
-#define VER 0.1
+#define VER 0.15
 enum Bool{True=1,False=0};
 
-int main(int argc,char *argv[]);
+int main(int argc,char *argv[]); 
 int Shell();
+void Run();
 using namespace std;
-string Codes[214753]; //字符串数组
+//char Codes[214753][214753];
+
+
 
 int Shell() {
-    cout << "Welcome to ECScript "<<VER<<"\n";
+    cout << "ECScript "<<VER<<"\n"<<"ECScript Shell is coming soon\n";
     return 0;
 }
 
-int main(int argc,char *argv[]) {
-    char s[214753];
+void Run(string code,int line) {
+    vector<string> codes;
+    if(code[0] == '#' || code == "") {
+        //cout << "commits" << endl;
+    } else {
+        char *tokenPtr = strtok((char*)code.c_str()," "); //分割字符串
+        while(tokenPtr != NULL) {
+            //cout << tokenPtr << endl;
+            codes.push_back(tokenPtr);
+            tokenPtr = strtok(NULL," ");
+        } 
+        if(codes[0] == "print") {
+            cout << codes[1] << endl;
+        }
+    }
+    return;
+}
+
+int main(int argc,char *argv[]) { //
+    char s[2147];
     vector<string> lines;
     if(argc == 1) {
         return Shell();
@@ -34,14 +55,15 @@ int main(int argc,char *argv[]) {
         } else {
          //   FILE a = fopen();
             ifstream f(argv[1],ios::in);
-            int i = 0; //计数
-            while(f.getline(s,214752)){ //读源文件内容
+            int line = 1; //计数
+            while(f.getline(s,2147)){ //读源文件内容
                 //cout << s << endl;
-                Codes[i] == s;
-                i++;
+                string s2(s);
+                Run(s2,line);
+                line++;
             }
             f.close();
-            
+            //Start();
         }
     }
 }
